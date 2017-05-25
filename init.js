b457b4aebe7a53277a4aff94025a38e309020935
sbfkcel <sbfkcel@163.com>
2017-05-25 10:55:33 +0800

    :zap:

--- init.js
+++ init.js
@@ -12,7 +12,7 @@ let key = data.RestrictionsOasswirdKey,
     let pass = ('0000' + star).slice(-4),
         code = crypto.pbkdf2Sync(pass,saltBase64,1000,20,'sha1').toString('hex');               
     console.log('正式尝试：' + pass);
-    if(code.toString() === keyBase64){
+    if(code === keyBase64){
         console.log('你的iOS访问限制密码是：' + pass);
     }else if(star < end){
         star++;
